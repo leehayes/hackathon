8a7fa4362c8b761115eb0e0bc136340bb753dee0
Lee Hayes <leehayes@gmail.com>
2017-11-15 07:47:49 +0000

    Add comments to example scrapers

--- scrape.py
+++ scrape.py
@@ -63,12 +63,14 @@ class Morris(Scraper):
     #http://www.wicket.space/walthamstuff?site=morris
     site = 'morris'
     def scrape(self):
+        #add scraping code here
         self.add_event(self.Event(event_url="http://www.wmgallery.org.uk/event1"))
 
 class Mill(Scraper):
     #http://www.wicket.space/walthamstuff?site=mill
     site = 'mill'
     def scrape(self):
+        #add scraping code here
         self.add_event(self.Event(event_url="http://themille17.org/event1",
                                     description="Stuff Happens"))
         self.add_event(self.Event(event_url="http://themille17.org/event2",
